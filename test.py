9fb91a88503a9abf8ddbefa1dd717f9bce8dfe87
Oldflat <ducksalt+github@gmail.com>
2021-01-31 11:57:55 -0500

    no comment

--- test.py
+++ test.py
@@ -11,14 +11,16 @@ import sys
 import pygame
 
 
+#draw bifurcation diagram
 def main():
     print('Hello World')
 
     pygame.init()
-    size = (1200, 800)
+    screen_size = (1200, 800)
 
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(screen_size)
 
+    # graph
 
     redraw = True
 
@@ -28,27 +30,25 @@ def main():
                 sys.exit()
 
         if redraw:
-            for ix in range(size[0]):
-                x = ix * 4 / size[0]
+            for ix in range(screen_size[0]):
+                x = ix * 4 / screen_size[0]
                 y_values = bifurcation_values(x)
-                iy_values = screenify_y_values(y_values, size[1])  # = int( 600 * (1-y))
+                iy_values = screenify_y_values(y_values, screen_size[1])  # = int( 600 * (1-y))
                 for iy in iy_values:
                     screen.set_at((ix, iy), (255,255,255))
 
                 pygame.display.update()
 
-
-            
             redraw = False
+
     
+'''returns a set of vertical values for the bifurcation diagram'''
 def bifurcation_values(r):
+    '''returns a set of vertical values for the bifurcation diagram'''
     y_values = []
 
-    # y_values.append(math.cos(r))
-    # y_values.append(math.sin(r))
-
-    iterations = 100
-    sample_threshold = 70
+    iterations = 300
+    sample_threshold = 200
 
     x = 0.5
 
@@ -61,6 +61,7 @@ def bifurcation_values(r):
 
 
 def screenify_y_values(y_values, height):
+    '''scales vertical values from the graph to the screen'''
     iy_values = []
     for y in y_values:
         iy_values.append(int(height * (1-y)))
@@ -69,5 +70,3 @@ def screenify_y_values(y_values, height):
 
 if __name__ == '__main__':
     main()
-
-
